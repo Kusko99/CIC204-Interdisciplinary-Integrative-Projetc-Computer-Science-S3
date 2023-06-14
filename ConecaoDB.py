372bf0ee4244c558084ad8ea09e44482dcd4aae7
Gustavo Yudji Hiromoto <natsugustavo@gmail.com>
2023-06-14 00:10:03 -0300

    backend e conecao do login e home

---
+++ ConecaoDB.py
@@ -0,0 +1,73 @@
+from mysql.connector import connection
+
+host ='localhost'
+port='3306'
+user ='user'
+password = 'password'
+database = 'db_MauaNet'
+
+
+def conexaoDB_Login(Email):
+
+    db_connection = connection.MySQLConnection(host=host, user=user, password=password,database=database)
+    cursor = db_connection.cursor()
+
+    query = "SELECT Senha FROM Aluno_Boletim where Email = %s"
+    values = [Email]
+    cursor.execute(query,values)
+    resultado = cursor.fetchall();
+    
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+    
+    return(resultado)
+
+def conexaoDB_Dados(Email):
+
+    db_connection = connection.MySQLConnection(host=host, user=user, password=password,database=database)
+    cursor = db_connection.cursor()
+
+    query = "SELECT Nome,Ra FROM Aluno_Boletim where Email = %s"
+    values = [Email]
+    cursor.execute(query,values)
+    resultado = cursor.fetchall();
+    
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+    
+    return(resultado)
+
+def conexaoDB_DadosRa(Ra):
+
+    db_connection = connection.MySQLConnection(host=host, user=user, password=password,database=database)
+    cursor = db_connection.cursor()
+    
+    query = "SELECT Grupo,Turma,Lab FROM GTL where Ra = %s"
+    values = [Ra]
+    cursor.execute(query,values)
+    resultado = cursor.fetchall();
+    
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+    
+    return(resultado)
+
+def conexaoDB_Notas(Ra):
+
+    db_connection = connection.MySQLConnection(host=host, user=user, password=password,database=database)
+    cursor = db_connection.cursor()
+    
+    query = "SELECT valor FROM Nota where Ra = %s"
+    values = [Ra]
+    cursor.execute(query,values)
+    resultado = cursor.fetchall();
+    
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+    
+    return(resultado)
+
--- maua_net_3.0.py
+++ maua_net_3.0.py
@@ -1,13 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import ConecaoDB
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route("/")
 def login():
     return render_template("login.html")
 
+@app.route("/login")
+def login_aux():
+    return render_template("login.html")
+
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    email = request.form['email']
+    senha = request.form['senha']
+    senha2 = ConecaoDB.conexaoDB_Login(email)
+    if(senha2==[]):
+        return redirect(url_for('login'))
+    else:
+        resultado = ConecaoDB.conexaoDB_Login(email)
+        senha2= resultado[0][0]
+        if (senha == senha2):
+            nome,ra,gtl = pegar_dados(email)
+            return redirect(url_for('mostrar_nome', email=email, nome=nome, ra=ra, gtl=gtl))
+        else:
+            flash('Email ou senha incorreto')
+    return redirect(url_for('login')) 
+    
+@app.route('/mostrar_nome/<email>/<nome>/<ra>/<gtl>')
+def mostrar_nome(email,nome,ra,gtl):
+    return render_template('home.html', email=email, nome=nome, ra=ra, gtl=gtl)
+
+@app.route('/processar', methods=['POST'])
+def processar():
+    nome = request.form['nome']
+    return redirect(url_for('mostrar_nome', nome=nome))
+
+@app.route('/pegar_dados')
+def pegar_dados(email):
+    resul = ConecaoDB.conexaoDB_Dados(email)
+    nome = resul[0][0]
+    Ra = resul[0][1]
+    resul2 = ConecaoDB.conexaoDB_DadosRa(Ra)
+    gtl = resul2[0][0] + resul2[0][1] + resul2[0][2]
+    return nome,Ra,gtl
 
 app.run(debug=True)
