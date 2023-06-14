955b63fd6bd9f73cb6a57ffa23ff8912e117879b
Kusko99 <fernandaofo120@gmail.com>
2023-06-14 10:33:10 -0300

    Começa da Pagina Materia

--- memedias/appMMedias.py
+++ memedias/appMMedias.py
@@ -13,7 +13,12 @@ def home():
     gtl = DbConnect.get_GTL_aluno(ra)
     id_materias = DbConnect.get_GTL_id_Materias(gtl)
     materias = DbConnect.get_nome_id_materias(id_materias)
-    return render_template('menu.html',materias=materias)
+    return render_template('menu.html', materias=materias)
+
+@app.route("/info_materia/<nome_materia>")
+def info_materia(nome_materia):
+    return render_template('materia.html', nome_materia=nome_materia)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
