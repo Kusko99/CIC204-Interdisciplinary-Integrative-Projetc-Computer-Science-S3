e0102062355b7fbc67c1b499eb74ec92282f9226
Kusko99 <fernandaofo120@gmail.com>
2023-06-14 09:43:43 -0300

    Primeiros passos do MMedias

---
+++ memedias/DbConnect.py
@@ -0,0 +1,63 @@
+from mysql.connector import connection
+
+host = 'localhost'
+user = 'root'
+password = 'portuguesa1'
+database = 'db_MauaNet'
+
+def get_GTL_aluno(ra):
+    db_connection = connection.MySQLConnection(host=host, user = user, password = password, database=database)
+    cursor = db_connection.cursor()
+
+    query = "SELECT GTL FROM GTL WHERE Ra = (%s)"
+    values = (ra,)
+    cursor.execute(query,values)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+
+    return resultados[0][0]
+
+def get_GTL_id_Materias(gtl):
+    db_connection = connection.MySQLConnection(host=host, user = user, password = password, database=database)
+    cursor = db_connection.cursor()
+
+    query = "SELECT DISTINCT Id_Materia FROM Aula WHERE GTL = (%s) ORDER BY Id_Materia; "
+    values = (gtl,)
+    cursor.execute(query,values)
+    resultados = cursor.fetchall()
+
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+
+    id_materias = []
+
+    for resultado in resultados:
+        id_materias.append(resultado[0])
+
+    return id_materias
+
+def get_nome_id_materias(lista_ids):
+    #Deve se entrar com uma lista contendo os ids
+    lista_nomes = []
+
+    for id in lista_ids:
+        db_connection = connection.MySQLConnection(host=host, user = user, password = password, database=database)
+        cursor = db_connection.cursor()
+
+        query = "SELECT nome FROM Materia WHERE Id_Materia = (%s)"
+        values = (id,)
+        cursor.execute(query,values)
+        resultado = cursor.fetchall()
+
+        cursor.close()
+        db_connection.commit()
+        db_connection.close()
+
+        lista_nomes.append(resultado[0][0])
+
+    return lista_nomes
+
---
+++ memedias/appMMedias.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, url_for
+import DbConnect
+
+app = Flask(__name__)
+
+@app.route('/')
+def standard():
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    ra = '19.00468-0'
+    gtl = DbConnect.get_GTL_aluno(ra)
+    id_materias = DbConnect.get_GTL_id_Materias(gtl)
+    materias = DbConnect.get_nome_id_materias(id_materias)
+    return render_template('menu.html',materias=materias)
+
+if __name__ == '__main__':
+    app.run(debug=True)
