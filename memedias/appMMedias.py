15157b99bba276911026c8a51be0e32b5fc6a6a3
Kusko99 <fernandaofo120@gmail.com>
2023-06-14 14:41:04 -0300

    Mais alterações

--- memedias/appMMedias.py
+++ memedias/appMMedias.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import DbConnect
 
 app = Flask(__name__)
@@ -23,11 +23,29 @@ def info_materia(nome_materia):
     quantidade_prova = DbConnect.get_quantidade_prova(id_materia)
     quantidade_trabalho = DbConnect.get_quantidade_trabalho(id_materia)
 
-    return render_template('materia.html', nome_materia=nome_materia, peso_prova = peso_prova, peso_trabalho = peso_trabalho,quantidade_trabalho = quantidade_trabalho,quantidade_prova = quantidade_prova)
+    return render_template('materia.html', nome_materia=nome_materia, peso_prova=peso_prova, peso_trabalho=peso_trabalho, quantidade_trabalho=quantidade_trabalho, quantidade_prova=quantidade_prova)
 
-@app.route("/minimo_esforco")
+@app.route("/minimo_esforco", methods=["GET", "POST"])
 def minimo_esforco():
+    quantidade_prova = int(request.args.get("quantidade_prova"))
+    quantidade_trabalho = int(request.args.get("quantidade_trabalho"))
+    if request.method == 'POST':
+        psub = request.form.get("psub")
+        inputs_prova = []
+        inputs_trabalho = []
+        for i in range(quantidade_prova):
+            input_prova = request.form.get("input{}".format(i))
+            inputs_prova.append(input_prova)
+        for i in range(quantidade_trabalho):
+            input_trabalho = request.form.get("input{}".format(i))
+            inputs_trabalho.append(input_trabalho)
+        
+        print("Psub:", psub)
+        print("Inputs Prova:", inputs_prova)
+        print("Inputs Trabalho:", inputs_trabalho)
     return ''
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
